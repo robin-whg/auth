a5ae98ac4f7ff0322e538cbbfb5628b20054b9d2
robin-whg <mail@robin-whg.com>
2021-03-26 09:40:04 +0100

    initial setup of firebase, nprogress, tailwind

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCjCBA88JhXkAJ_IXds-fl4AL01N5MbESY",
+  authDomain: "auth-87f60.firebaseapp.com",
+  databaseURL: "https://auth-87f60.firebaseio.com",
+  projectId: "auth-87f60",
+  storageBucket: "auth-87f60.appspot.com",
+  messagingSenderId: "140444113176",
+  appId: "1:140444113176:web:b8b0a9f4c8289debe9a66d",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth()
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-color": "var(--primary-color)",
+      }
+    },
   },
   variants: {
     extend: {},
