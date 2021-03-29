e3330b692ed85ffa6e6ee5b69efd035791d37491
robin-whg <mail@robin-whg.com>
2021-03-29 16:43:00 +0200

    reroute after sign out if view requires auth

--- src/components/TheNavUser.vue
+++ src/components/TheNavUser.vue
@@ -1,30 +1,48 @@
 <template>
   <div v-if="user">
-    <base-button @click="signOut" class="bg-red-200 text-red-600">Sign Out</base-button>
+    <base-button @click="signOut" class="bg-red-200 text-red-600"
+      >Sign Out</base-button
+    >
   </div>
   <div v-else>
-    <base-button @click="$router.push({name: 'SignIn'})" class="hover:bg-gray-100 mr-2">Sign In</base-button>
-    <base-button @click="$router.push({name: 'SignUp'})" class="bg-blue-600 text-gray-50">Sign Up</base-button>
+    <base-button
+      @click="$router.push({ name: 'SignIn' })"
+      class="hover:bg-gray-100 mr-2"
+      >Sign In</base-button
+    >
+    <base-button
+      @click="$router.push({ name: 'SignUp' })"
+      class="bg-blue-600 text-gray-50"
+      >Sign Up</base-button
+    >
   </div>
 </template>
 
 <script>
-import { auth, state } from '@/firebase.js'
-import { computed } from 'vue'
+import { auth, state } from "@/firebase.js";
+import { useRouter, useRoute } from "vue-router";
+import { computed } from "vue";
 
 export default {
-    setup() {
-        const user = computed(() => { return state.user })
-        async function signOut() {
-            try {
-                await auth.signOut()
-            } catch (error) {
-                console.log(error)
-            }
+  setup() {
+    const router = useRouter();
+    const route = useRoute()
+    const user = computed(() => {
+      return state.user;
+    });
+    async function signOut() {
+      try {
+        await auth.signOut();
+        if(route.meta.requiresAuth) {
+          router.push({ name: 'Home' })
         }
-        return { user, signOut }
+      } catch (error) {
+        console.log(error);
+      }
     }
-}
+    return { user, signOut };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
