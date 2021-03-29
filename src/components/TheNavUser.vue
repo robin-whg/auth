bb5be30ec543f76ea98712a1df766e2fef09a281
robin-whg <mail@robin-whg.com>
2021-03-29 16:22:02 +0200

    navbar buttons for auth conditionally showing

--- src/components/TheNavUser.vue
+++ src/components/TheNavUser.vue
@@ -1,14 +1,30 @@
 <template>
-  <div v-if="true">
-    <base-button class="bg-red-600 text-gray-50">Sign Out</base-button>
+  <div v-if="user">
+    <base-button @click="signOut" class="bg-red-200 text-red-600">Sign Out</base-button>
   </div>
   <div v-else>
-    <base-button>Sign In</base-button>
-    <base-button class="bg-blue-600">Sign Up</base-button>
+    <base-button @click="$router.push({name: 'SignIn'})" class="hover:bg-gray-100 mr-2">Sign In</base-button>
+    <base-button @click="$router.push({name: 'SignUp'})" class="bg-blue-600 text-gray-50">Sign Up</base-button>
   </div>
 </template>
 
 <script>
+import { auth, state } from '@/firebase.js'
+import { computed } from 'vue'
+
+export default {
+    setup() {
+        const user = computed(() => { return state.user })
+        async function signOut() {
+            try {
+                await auth.signOut()
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        return { user, signOut }
+    }
+}
 </script>
 
 <style lang="scss" scoped>
