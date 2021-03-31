09a460648ff91e5e2e9258178c65fa223cf52c5e
robin-whg <mail@robin-whg.com>
2021-03-31 09:31:37 +0200

    loading animation for base button component

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -4,7 +4,20 @@
     :class="{ 'cursor-default': loading }"
     class="py-2 px-4 font-semibold rounded-lg focus:outline-none focus:ring-none"
   >
-    <slot />
+    <div class="flex items-center justify-center">
+      <span :class="{ slot: loading }">
+        <slot />
+      </span>
+      <span v-if="loading" class="absolute z-10 w-full">
+        <svg
+          class="animate-spin mx-auto"
+          viewBox="0 0 100 100"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <circle cx="50" cy="50" r="45" />
+        </svg>
+      </span>
+    </div>
   </button>
 </template>
 
@@ -20,4 +33,18 @@ export default {
 </script>
 
 <style scoped>
+.slot {
+  visibility: hidden;
+}
+svg {
+  max-width: 24px;
+}
+circle {
+  fill: transparent;
+  stroke: currentColor;
+  stroke-width: 8;
+  stroke-dasharray: 214;
+  stroke-linecap: round;
+  transform-origin: 50% 50%;
+}
 </style>
