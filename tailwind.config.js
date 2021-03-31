f5e05f256380720595cdbefa128649b202db9566
robin-whg <mail@robin-whg.com>
2021-03-31 09:29:35 +0200

    different gray tone

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
+const colors = require('tailwindcss/colors')
 module.exports = {
   purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
   darkMode: 'class', // or 'media' or 'class'
   theme: {
+    colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        black: colors.black,
+        white: colors.white,
+        gray: colors.trueGray,
+        blue: colors.blue,
+        green: colors.green,
+        indigo: colors.indigo,
+        red: colors.rose,
+        yellow: colors.amber,
+      },
     extend: {
       colors: {
         "primary-color": "var(--primary-color)",
