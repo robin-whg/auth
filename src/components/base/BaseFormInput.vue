245b2300d0a7ddba9ab07405110cd764da0df1ab
robin-whg <mail@robin-whg.com>
2021-04-12 16:43:19 +0200

    no border to make it as high as buttons

--- src/components/base/BaseFormInput.vue
+++ src/components/base/BaseFormInput.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="pb-4 border-0 px-0 bg-transparent">
-    <label class="font-semibold" v-if="label">{{ label }}</label>
+  <div class="border-0 px-0 bg-transparent">
+    <label class="font-semibold" :class="{ 'sr-only' : srOnly }" v-if="label">{{ label }}</label>
     <span class="float-right">
       <slot />
     </span>
@@ -8,7 +8,7 @@
       v-bind="$attrs"
       :value="modelValue"
       @input="$emit('update:modelValue', $event.target.value)"
-      class="w-full rounded-lg hover:bg-transparent hover:border-gray-200 border-2 dark:hover:border-gray-700 focus:bg-transparent bg-gray-200  dark:bg-gray-700 focus:ring-0 focus:border-gray-200 dark:focus:border-gray-700"
+      class="w-full rounded-lg hover:bg-transparent hover:border-gray-200 border-0 dark:hover:border-gray-700 focus:bg-transparent bg-gray-200  dark:bg-gray-700 focus:ring-0 focus:border-gray-200 dark:focus:border-gray-700"
       :class="[
         error ? 'border-red-500' : 'border-gray-200 dark:border-gray-700',
       ]"
@@ -32,6 +32,10 @@ export default {
       type: String,
       default: "",
     },
+    srOnly: {
+      type: Boolean,
+      default: false
+    }
   },
 };
 </script>
