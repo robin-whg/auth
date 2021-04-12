83a4cc0939f0e6f383d506ed27f6ec5f0b4ecd60
robin-whg <mail@robin-whg.com>
2021-04-12 16:43:44 +0200

    table for users

---
+++ src/modules/admin/components/UserListItem.vue
@@ -0,0 +1,118 @@
+<template>
+  <tr>
+    <td class="px-6 py-4 whitespace-nowrap">
+      <div class="flex items-center">
+        <div class="text-secondary rounded-full bg-gray-100 h-10 w-10 flex justify-center items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+            />
+          </svg>
+        </div>
+        <div class="ml-4">
+          <div class="text-sm font-medium text-gray-900">
+            {{ user.displayName }}
+          </div>
+          <div class="text-sm text-gray-500">{{ user.email }}</div>
+        </div>
+      </div>
+    </td>
+    <td class="px-6 py-4 whitespace-nowrap">
+      <div class="text-sm text-gray-900">{{ user.uid }}</div>
+    </td>
+    <td class="px-6 py-4 whitespace-nowrap">
+      <base-badge v-if="!user.disabled" class="alert-success">
+        Active
+      </base-badge>
+      <base-badge v-else class="alert-danger">
+        Disabled
+      </base-badge>
+    </td>
+    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+      <template v-if="user.customClaims">
+        <base-badge
+          v-for="claim in Object.keys(user.customClaims)"
+          :key="claim"
+          class="alert-warning"
+        >
+          {{ claim }}
+        </base-badge>
+      </template>
+    </td>
+    <td class="px-6">
+      <div class="flex justify-end space-x-4">
+        <base-button pill class="link-secondary" size="sm">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2"
+            />
+          </svg>
+        </base-button>
+        <base-button pill class="link-secondary" size="sm">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+            />
+          </svg>
+        </base-button>
+        <base-button pill class="link-danger" size="sm">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+            />
+          </svg>
+        </base-button>
+      </div>
+    </td>
+  </tr>
+</template>
+
+<script>
+export default {
+  props: {
+    user: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/modules/admin/views/UserManagement.vue
+++ src/modules/admin/views/UserManagement.vue
@@ -1,28 +1,110 @@
 <template>
-    <div>
-manage
-<base-button @click="listUsers()" class="btn-primary">load users</base-button>
-<pre>{{ users }}</pre>
+  <div class="container p-4 mx-auto">
+    <div class="flex flex-col">
+      <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div class="shadow-sm overflow-hidden border-gray-200 rounded-xl">
+            <div class="bg-gray-50 flex items-center space-x-4 py-2 px-4">
+              <base-form-input
+                class="w-full"
+                v-model.trim="searchQuery"
+                label="Search"
+                srOnly
+                type="text"
+                placeholder="Search for name, email, uid or role"
+              ></base-form-input>
+              <base-button class="btn-primary">
+                <span>Add User</span>
+              </base-button>
+              <base-button class="link-secondary">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+                  />
+                </svg>
+              </base-button>
+            </div>
+            <table class="min-w-full divide-y divide-gray-200">
+              <thead class="bg-gray-50">
+                <tr>
+                  <th
+                    scope="col"
+                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Name
+                  </th>
+                  <th
+                    scope="col"
+                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    UID
+                  </th>
+                  <th
+                    scope="col"
+                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Status
+                  </th>
+                  <th
+                    scope="col"
+                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Roles
+                  </th>
+                  <th scope="col" class="relative px-6 py-3">
+                    <span class="sr-only">Edit</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody class="bg-gray-50 divide-y divide-gray-200">
+                <user-list-item
+                  v-for="user in users"
+                  :key="user.uid"
+                  :user="user"
+                />
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import { listUsers as _listUsers } from '../admin.service.js'
-    export default {
-        data() {
-            return {
-                users: []
-            }
-        },
-        methods: {
-            async listUsers() {
-                const users = await _listUsers()
-                this.users = users
-            }
-        },       
-    }
+import * as service from "../admin.service.js";
+import UserListItem from "../components/UserListItem.vue";
+export default {
+  components: {
+    UserListItem,
+  },
+  data() {
+    return {
+      users: [],
+      searchQuery: "",
+    };
+  },
+  methods: {
+    async listUsers() {
+      const { data } = await service.listUsers();
+      this.users = data.users;
+      console.log(data.users);
+    },
+  },
+  async created() {
+    this.listUsers();
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
