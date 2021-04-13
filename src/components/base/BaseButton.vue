a899690375646bef52fa196aaf711b39ed370576
robin-whg <mail@robin-whg.com>
2021-04-13 10:08:47 +0200

    rounded prop to have circular buttons

--- src/components/base/BaseButton.vue
+++ src/components/base/BaseButton.vue
@@ -4,9 +4,12 @@
     :class="[
       { 'cursor-default': loading },
       pill ? 'rounded-full' : 'rounded-lg',
-      { 'py-1 px-2 text-sm': size === 'sm' },
-      { 'py-2 px-3': size === 'md' },
-      { 'py-3 px-4 text-xl': size === 'lg' },
+      { 'py-1 px-2 text-sm': size === 'sm' && !rounded },
+      { 'py-2 px-3': size === 'md'  && !rounded},
+      { 'py-3 px-4 text-xl': size === 'lg' && !rounded },
+      { 'p-1 rounded-full text-sm': size === 'sm' && rounded },
+      { 'p-2 rounded-full': size === 'md' && rounded },
+      { 'p-3 rounded-full text-xl': size === 'lg' && rounded },
     ]"
     class="font-semibold flex items-center justify-center"
   >
@@ -32,6 +35,10 @@ export default {
       type: String,
       default: "md",
     },
+    rounded: {
+      type: Boolean,
+      default: false
+    }
   },
 };
 </script>
