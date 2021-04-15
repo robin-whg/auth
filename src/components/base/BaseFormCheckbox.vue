fff995ea5105dac46c53caafd25472babb34813b
robin-whg <mail@robin-whg.com>
2021-04-15 14:53:49 +0200

    base component for checkboxes

---
+++ src/components/base/BaseFormCheckbox.vue
@@ -0,0 +1,32 @@
+<template>
+  <label class="flex items-center select-none">
+    <input
+      type="checkbox"
+      :checked="modelValue"
+      @change="$emit('update:modelValue', $event.target.checked)"
+      class="rounded mr-2"
+      :class="[modelValue ? 'border-primary-500 bg-primary-500' : 'border-gray-200 bg-gray-200 dark:border-gray-700 dark:bg-gray-700']"
+    />
+    <span class="font-semibold" v-if="label">
+      {{ label }}
+    </span>
+  </label>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: "",
+    },
+    modelValue: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
