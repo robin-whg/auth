2c8165a4b21332a93a762a89faddff9e392bfaa9
robin-whg <mail@robin-whg.com>
2021-04-15 14:54:57 +0200

    hide default buttons on number input

--- src/components/base/BaseFormInput.vue
+++ src/components/base/BaseFormInput.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="border-0 px-0 bg-transparent">
-    <label class="font-semibold" :class="{ 'sr-only' : srOnly }" v-if="label">{{ label }}</label>
+    <label class="font-semibold" :class="{ 'sr-only': srOnly }" v-if="label">{{
+      label
+    }}</label>
     <span class="float-right">
       <slot />
     </span>
@@ -8,7 +10,7 @@
       v-bind="$attrs"
       :value="modelValue"
       @input="$emit('update:modelValue', $event.target.value)"
-      class="w-full rounded-lg hover:bg-transparent hover:border-gray-200 border-0 dark:hover:border-gray-700 focus:bg-transparent bg-gray-200  dark:bg-gray-700 focus:ring-0 focus:border-gray-200 dark:focus:border-gray-700"
+      class="w-full rounded-lg hover:bg-transparent hover:border-gray-200 border-0 dark:hover:border-gray-700 focus:bg-transparent bg-gray-200 dark:bg-gray-700 focus:ring-0 focus:border-gray-200 dark:focus:border-gray-700"
       :class="[
         error ? 'border-red-500' : 'border-gray-200 dark:border-gray-700',
       ]"
@@ -34,8 +36,22 @@ export default {
     },
     srOnly: {
       type: Boolean,
-      default: false
-    }
+      default: false,
+    },
   },
 };
 </script>
+
+<style scoped>
+/* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+/* Firefox */
+input[type="number"] {
+  -moz-appearance: textfield;
+}
+</style>
