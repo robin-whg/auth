b8c9d8ff60062e3f4d67fd709a332080c4c4fcdd
robin-whg <mail@robin-whg.com>
2021-04-16 14:56:13 +0200

    no whitespace wrapping

--- src/components/layout/TheNavbarAvatarDarkModeToggle.vue
+++ src/components/layout/TheNavbarAvatarDarkModeToggle.vue
@@ -3,11 +3,11 @@
     @click="toggle()"
     class="text-left w-full px-4 py-1 flex justify-between items-center hover:bg-gray-200 dark:hover:bg-gray-700"
   >
-    <span class="flex items-center" v-if="darkMode">
+    <span class="flex whitespace-nowrap items-center" v-if="darkMode">
       <base-icon name="sun" class="h-6 w-6 text-yellow-300 mr-2" />
       Light Mode
     </span>
-    <span class="flex items-center" v-else>
+    <span class="flex items-center whitespace-nowrap" v-else>
       <base-icon name="moon" class="h-6 w-6 text-gray-500 mr-2" />
       Dark Mode
     </span>
