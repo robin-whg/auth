342fbda6229704acfafa43208f125689ae4ba2c2
robin-whg <mail@robin-whg.com>
2021-04-23 11:12:42 +0200

    component to be displayed when the current user is not supposed to see the page

---
+++ src/components/base/BaseDeny.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex container flex-col items-center p-4">
+    <base-icon class="text-danger" name="lock-closed" size="h-24 w-24" />
+    <h1 class="mb-2">Access Denied</h1>
+    <base-alert class="sm:max-w-md" type="danger">
+      User is not authorized to view this page. 
+    </base-alert>
+ </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
